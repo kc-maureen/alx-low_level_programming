1e5472f9e15f2a35ce54ce4176d71c4694b5f9d4
kc-maureen <maureenchepngeno34@gmail.com>
2021-06-17 13:21:36 -0700

    c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+* main - Prints this  "with proper grammar, but the outcome is a piece of art,"
+*
+* Return: 0 to whomever called it.
+*/
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
