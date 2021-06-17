1f51f9d7212ff7e47162537ff1095952b45ed434
kc-maureen <maureenchepngeno34@gmail.com>
2021-06-17 13:05:16 -0700

    c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+* main - This function takes no arguments
+* Prints "Programming is like building a multilingual puzzle
+*
+* Return: 0
+*/
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
