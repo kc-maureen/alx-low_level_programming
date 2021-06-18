cd9bb8f4fa22c1751b57fd5f94709c29031e2c90
kc-maureen <maureenchepngeno34@gmail.com>
2021-06-18 09:29:02 -0700

    ifelsewhile

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ * Description: Prints out the alphabet in lowercase and in uppercase
+ * Return: returns 0 if successful
+ */
+int main(void)
+{
+char ph, ph1;
+
+for (ph = 'a'; ph <= 'z'; ph++)
+{
+putchar(ph);
+}
+for (ph1 = 'A'; ph2 <= 'Z'; ph++)
+{
+putchar(ph1);
+}
+putchar('\n');
+return (0);
+}
